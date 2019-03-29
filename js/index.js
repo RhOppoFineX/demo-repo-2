f56ca26aa3de3682112b9c837ccc3f60d3662f98
Diego Arias <d.arias2000.eh@gmail.com>
2019-03-28 18:18:26 -0600

    Commit Inicial

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/*Esta funcion se ejecuta cuadndo se termine de cargar el sitio */
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()/*Llamamos a la funcion tooltip sobre todos los elementos
+     que tengan como atributo: data-toggle="tooltip"*/
+  })
+
+  /*a este tipo de archivos se le llaman plugin de JQuery */ 
